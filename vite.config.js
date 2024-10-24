39033cfb08b46ca40f5c05eb8ad18e08487c644c
Aditya Kumar Mishra <89987516+Adityamishra114@users.noreply.github.com>
2024-10-24 22:33:42 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -21,9 +21,6 @@ export default defineConfig({
         },
       },
     },
-    chunkSizeWarningLimit: 1000, 
-  },
-  server: {
-    port: 3000, 
+    chunkSizeWarningLimit: 1000, // Warning limit for chunk size
   },
 });
